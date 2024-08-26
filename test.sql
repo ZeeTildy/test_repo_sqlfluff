5904d840357fffe69b061cc0b4c1b22aa16a02ff
zsolt-tildy_mars <zsolt.tildy@effem.com>
2024-08-26 10:42:17 -0400

    initial commit

---
+++ test.sql
@@ -0,0 +1,14 @@
+INSERT INTO 
+WITH cte AS (
+    SELECT DISTINCT
+        canonical_id,
+        FROM_UNIXTIME(first_created_date) AS first_created_date
+    FROM email_attributes
+)
+
+SELECT
+    COUNT(canonical_id) AS cosnumer_count,
+    DATE(first_created_date) AS day_mailed
+FROM cte
+GROUP BY 2
+ORDER BY 2 DESC
